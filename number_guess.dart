f730ab9314f6d31b6f1f84ae406829df1cf0a821
Hoe Wei Leong <weileonghoe@gmail.com>
2025-06-01 12:06:19 +0800

    add basic user input and basic function definition

--- number_guess.dart
+++ number_guess.dart
@@ -1,3 +1,19 @@
 // Computer will pick a random number between user selected range ( 0 - 100 )
 // And the user will be trying to guess it, the program will tells if it is too low, too high and correct
 // The game continues after user guess the corrects number
+
+import 'dart:io';
+
+void main(){
+  print("Enter Range ( Low ) : ");
+  int? low = int.parse(stdin.readLineSync()!); // stdin.readLineSync()! means this will be accepting a value
+  
+  print("Enter Range ( High ) : ");
+  int? high = int.parse(stdin.readLineSync()!); // stdin.readLineSync()! means this will be accepting a value
+
+}
+
+void execution(int low, int high){
+
+}
+
