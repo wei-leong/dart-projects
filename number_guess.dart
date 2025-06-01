9b7f7fd5ae0de3b4a3587ee78fc84fcba3757ad1
Hoe Wei Leong <weileonghoe@gmail.com>
2025-06-01 11:58:31 +0800

    add projects requirements to file

---
+++ number_guess.dart
@@ -0,0 +1,3 @@
+// Computer will pick a random number between user selected range ( 0 - 100 )
+// And the user will be trying to guess it, the program will tells if it is too low, too high and correct
+// The game continues after user guess the corrects number
