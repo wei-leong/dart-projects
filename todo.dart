5e856717acde7af0cd45e959356adb30efb1c8cd
Hoe Wei Leong <weileonghoe@gmail.com>
2025-06-02 11:53:28 +0800

    modify viewMenu function and add switch statement into function

--- todo.dart
+++ todo.dart
@@ -14,8 +14,26 @@ Map<String,bool> todo = {
 };
 
 void main(){
-  int returnedInput = menuReturn();
-  switch(returnedInput){
+  menu();
+}
+
+void menu(){
+  String menu = 
+  """
+
+Todo List CLI Menu
+__________________
+1. Add Task
+2. View Task 
+3. Edit Task
+4. Mark Done
+5. Remove Task
+  """;
+
+  print(menu);
+  stdout.write("Type number here: ");
+  int? input = int.parse(stdin.readLineSync()!);
+    switch(input){
     case 1:
       viewTask();
       break;
@@ -37,24 +55,6 @@ void main(){
   }
 }
 
-int menuReturn(){
-  String menu = 
-  """
-Todo List CLI Menu
-__________________
-1. Add Task
-2. View Task 
-3. Edit Task
-4. Mark Done
-5. Remove Task
-  """;
-
-  print(menu);
-  stdout.write("Type number here: ");
-  int? input = int.parse(stdin.readLineSync()!);
-  return input;
-}
-
 void viewTask(){
   int i = 1;
   for(var task in todo.entries){ // Entries includes keys and values
@@ -63,4 +63,5 @@ void viewTask(){
     }
     i++;
   }
+  menu();
 }
