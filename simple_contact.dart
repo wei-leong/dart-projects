7e87b495c727765f64c9ac21dcd9f3b9eddf9d7a
Hoe Wei Leong <weileonghoe@gmail.com>
2025-06-03 11:41:54 +0800

    feat: menu for simple contact list

--- simple_contact.dart
+++ simple_contact.dart
@@ -5,3 +5,65 @@
 // 3. Add New Contact
 // 4. Update Phone Number 
 // 5. Delete A Contact
+import 'dart:io';
+
+void main(){
+  createMenu();
+}
+
+void createMenu(){
+  int menuItems = 5;
+  String menu = 
+"""
+Simple Contact Menu
+1. View All Contact
+2. Search Contact 
+3. Add New Contact
+4. Update Phone Number
+5. Delete A Contact
+
+""";
+
+  print(menu);
+  
+  // Receive Input from User
+  int? input = userInput(menuItems);
+  print("User Input : $input");
+  
+
+
+}
+
+int? userInput(int menuItems){
+  // Variable for Loop
+  bool isInt = false;
+  int? inputNum = 0;
+
+  while(!isInt){
+    // User Input 
+    stdout.write("Please enter a number : ");
+    inputNum = int.tryParse(stdin.readLineSync()!);
+
+    if(inputNum != null){
+      if(inputNum <= 0 || inputNum > menuItems){
+        print("Invalid number range, please try again");
+        space();
+        continue;
+      }
+      else{
+        isInt = true;
+        break;
+      }
+    }
+    else{
+      print("Invalid number format, please try again");
+      space();
+      continue;
+    }
+  }
+  return inputNum;
+}
+
+void space(){
+  print(" ");
+}
