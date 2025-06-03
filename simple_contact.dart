8641e1bbced994980fd717768ef457b8e1fa1443
Hoe Wei Leong <weileonghoe@gmail.com>
2025-06-03 11:48:02 +0800

    add switch statement in menu

--- simple_contact.dart
+++ simple_contact.dart
@@ -38,10 +38,19 @@ Simple Contact Menu
   
   // Receive Input from User
   int? input = userInput(menuItems);
-  print("User Input : $input");
   
-
-
+  switch(input){
+    case 1:
+      break;
+    case 2:
+      break;
+    case 3:
+      break;
+    case 4:
+      break;
+    case 5:
+      break;
+  }
 }
 
 int? userInput(int menuItems){
