4db61d192dc9c5a4b3c0159038de8c2bf8dc5311
Hoe Wei Leong <weileonghoe@gmail.com>
2025-06-03 11:21:26 +0800

    create new file for new project ( Simple Contact List )

---
+++ simple_contact.dart
@@ -0,0 +1,7 @@
+// This project is about creating a Simple Contact Book
+// User Can : 
+// 1. View All Contact
+// 2. Search Contact With A Specific Name
+// 3. Add New Contact
+// 4. Update Phone Number 
+// 5. Delete A Contact
