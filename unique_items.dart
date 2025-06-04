4f811c5b6c68d8268e7c6100351ff51b4cbdb0ac
Hoe Wei Leong <weileonghoe@gmail.com>
2025-06-04 11:38:04 +0800

    feat: add loop to collect unique items and show count

--- unique_items.dart
+++ unique_items.dart
@@ -1,8 +1,43 @@
-// Write a program to repeatedly ask the user to enter a number, then it is added with set for uniqueness
+// Write a program to repeatedly ask the user to enter a word or number, then it is added with set for uniqueness
 // After each entry provide a menu, show how many unique items so for with length
 // When user type E ( Other Alphabet ), it will stop the loop and print the set alphabetically
 // Tips : Since Set doesn't have sort function, we need to convert to List and use the sort method
 
-void main(){
+import 'dart:io';
+import 'simple_contact.dart';
 
+// Global Variable
+Set<String> uniqueItems = {};
+
+void main(){
+  repeatInput();
+}
+
+void repeatInput(){
+  bool isTerminated = false;
+  while(!isTerminated){
+    String input = checkInput();
+    if(input == "exit"){
+      isTerminated = true;
+      break;
+    }else{
+      uniqueItems.add(input);
+      print("Number of Unique Items : ${uniqueItems.length}");
+      space();
+      continue;
+    }
+  }
+  space();
+  print(uniqueItems);
+  print("Program ends");
+}
+
+String checkInput(){
+  stdout.write("Input Value to Check for Uniqueness : ");
+  String? userInput = stdin.readLineSync()!;
+  return userInput;
+}
+
+void space(){
+  print(" ");
 }
