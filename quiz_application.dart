a8885c982f5686b6229b083beee009e7c2b84669
Hoe Wei Leong <weileonghoe@gmail.com>
2025-06-04 12:12:17 +0800

    chore(new project)!: initialize Quiz Application Project

---
+++ quiz_application.dart
@@ -0,0 +1,7 @@
+// Create a Console Quiz Application that stores a 5 questions
+// Create 2 Maps ( Questions Map<String,<List<String>>, Answer Map<String,String>)
+// Print Out the Score ( n/5 ) after user finished answering the questions 
+
+void main(){
+  
+}
