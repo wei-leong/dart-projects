7e6e3559a0430073688ee8a69727783195174254
Hoe Wei Leong <weileonghoe@gmail.com>
2025-06-04 11:24:08 +0800

    chore: modify practice requirements and added void main function

--- unique_items.dart
+++ unique_items.dart
@@ -1,2 +1,8 @@
 // Write a program to repeatedly ask the user to enter a number, then it is added with set for uniqueness
-// After each entry provide a menu 
+// After each entry provide a menu, show how many unique items so for with length
+// When user type E ( Other Alphabet ), it will stop the loop and print the set alphabetically
+// Tips : Since Set doesn't have sort function, we need to convert to List and use the sort method
+
+void main(){
+
+}
