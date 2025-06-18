386b57e9829096fc5ae3af71c51f474ac3801a42
Hoe Wei Leong <weileonghoe@gmail.com>
2025-06-18 11:41:26 +0800

    feat: add menu for calculator.dart

---
+++ calculator.dart
@@ -0,0 +1,19 @@
+import 'dart:io';
+
+int? savedNum; 
+
+void main(){
+  while(true){
+    String message = 
+"""
+Basic Calculator App
+____________________
+1. Addition
+2. Subtraction
+3. Multiplication
+4. Division
+5. Clear Saved Numbers
+6. Exit
+""";
+  }
+}
