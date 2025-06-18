0bc983010625abc8863cb905b93fc1dd3ea774a2
Hoe Wei Leong <weileonghoe@gmail.com>
2025-06-18 17:27:29 +0800

    feat: add multiplication function to calculator

--- calculator.dart
+++ calculator.dart
@@ -18,6 +18,7 @@ void main(){
         subtraction();
         break;
       case 3:
+        multiplication();
         break;
       case 4:
         break;
@@ -74,6 +75,23 @@ void subtraction(){
     savedNum -= secondValue;
     print("\nSubtraction Value");
     print("$pastSaveState - $secondValue = $savedNum" + "\n");
+  }
+}
+
+void multiplication(){
+  if(savedNum == 0){
+    int firstValue = checkNumInput(msg:"\nPlease enter 1st Value : ");
+    int secondValue = checkNumInput(msg:"\nPlease enter 2nd Value : ");
+
+    savedNum = firstValue * secondValue;
+    print("\nMultiplication Value");
+    print("$firstValue x $secondValue = $savedNum" + "\n");
+  }else{
+    int pastSaveState = savedNum;
+    int secondValue = checkNumInput(msg:"\n" + "$savedNum x ");
+    savedNum *= secondValue;
+    print("\nMultiplication Value");
+    print("$pastSaveState x $secondValue = $savedNum" + "\n");
   }
 }
 
