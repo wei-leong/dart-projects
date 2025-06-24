f2716bc8570bf0c00cf18fd88c6992a4dc8ec46f
Hoe Wei Leong <weileonghoe@gmail.com>
2025-06-24 13:04:43 +0800

    feat: add deposit and withdrawal feature

--- bank_system.dart
+++ bank_system.dart
@@ -1,4 +1,47 @@
 // Create a banking system using Class 
-// 1. Display Account Information
-// 2. Deposit
-// 3. Withdrawal
+// 1. Login
+// 2. Display Account Information
+// 3. Deposit
+// 4. Withdrawal
+
+import 'dart:io';
+
+void main(){
+  Bank john = new Bank("John Smith" , "12345678" , 11987.80);
+}
+
+class Bank{
+  // Attributes
+  String accountName;
+  String password;
+  double currentBalance;
+
+  Bank(this.accountName,this.password,this.currentBalance);
+
+  void deposit(){
+    print("Current Balance : $currentBalance");
+    double depositAmount = checkDoubleInput( msg : "Please enter a amount to Deposit : ");
+    currentBalance += depositAmount;
+    print("\n" + "Current Balance : $currentBalance");
+  }
+
+  void withdrawal(){
+    print("Current Balance : $currentBalance");
+    double withdrawalAmount = checkDoubleInput( msg : "Please enter a amount to Withdrawal : ");
+    currentBalance -= withdrawalAmount;
+    print("\n" + "Current Balance : $currentBalance");
+  }
+
+  double checkDoubleInput({String msg = "Please enter a amount : "}) {
+    while (true) {
+      stdout.write(msg);
+      double? userInput = double.tryParse(stdin.readLineSync()!);
+      if (userInput != null) {
+        return userInput;
+      } else {
+        print("Invalid Input, please try again \n");
+      }
+    }
+  }
+}
+
